e7b9b07128a3e548dc2a46745a4bcba78165fe05
sergio bodean <sereoga24@hotmail.com>
2023-06-07 12:39:04 +0100

    added stimulus controller for map

--- app/controllers/lands_controller.rb
+++ app/controllers/lands_controller.rb
@@ -1,13 +1,9 @@
 class LandsController < ApplicationController
-
   def index
   end
 
   def set_location
     session[:location] = params[:land][:address]
-  end
-
-  def set_total_area
-    @land = Land.new
+    raise
   end
 end
