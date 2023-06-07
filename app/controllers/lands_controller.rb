60f8811fc0456b00542d23497686d0021f833581
Fabien Mendez <fabien.mendez@gmail.com>
2023-06-07 17:42:42 +0100

    solved a bug with land controller

--- app/controllers/lands_controller.rb
+++ app/controllers/lands_controller.rb
@@ -4,6 +4,7 @@ class LandsController < ApplicationController
     @carbon = CarbonCreditPrice.last
     @carbon_price = CreditPerHa.last
     @total = @carbon.year_1 * @carbon_price.year_1
+  end
 
   def index
   end
