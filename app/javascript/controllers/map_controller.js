5fd0af447c113e859a0cfd6c10dc656766ce8d03
sergio bodean <sereoga24@hotmail.com>
2023-06-08 12:12:04 +0100

    made map resizing depending on the choice of the person

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,6 +1,6 @@
 // app/javascript/controllers/map_controller.js
 import { Controller } from "@hotwired/stimulus"
-// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 export default class extends Controller {
   static values = {
@@ -16,16 +16,38 @@ export default class extends Controller {
       style: "mapbox://styles/mapbox/streets-v10"
     })
 
-    this.#fitMapToMarkers()
+    this.geocoder = new MapboxGeocoder({
+      accessToken: this.apiKeyValue,
+      types: "country,region,place,address"
+    })
+    this.geocoder.on("result", event => this.#setInputValue(event))
+    this.geocoder.on("clear", () => this.#clearInputValue())
+    this.geocoder.addTo(this.element)
+
+
     // this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
     //   mapboxgl: mapboxgl }))
   }
 
-  #fitMapToMarkers() {
-    this.map.fitBounds([
-      [-79.686915, -12.144785], // southwestern corner of the bounds -12.144785, -63.686915
-      [-38.160500, -7.984657] // northeastern corner of the bounds -7.984657, -35.160500
-      ], { padding: 100, maxZoom: 20, duration: 3 });
+  #setInputValue(event) {
+    console.log(event.result)
+    // event.result["place_name"].replace(/^(.*?),(.*)$/, "$1");
+    // const str = event.result["place_name"].replace(/^(.*?),(.*)$/, "$1");
+    // this.addressTarget.value = event.result["place_name"].replace(/^(.*?),(.*)$/, "$1");
+
+      new mapboxgl.Marker()
+      .setLngLat([event.result.center[0], event.result.center[1]])
+      .addTo(this.map)
+      this.map.flyTo({
+        center: [event.result.center[0], event.result.center[1]],
+       // this animation is considered essential with respect to prefers-reduced-motion
+        zoom: 10
+      });
+      lastloc = [event.result.center[0], event.result.center[1]]
+  }
+
+  #clearInputValue() {
+    this.addressTarget.value = ""
   }
 
 }
