bb14b58a530b3c80595dcfdff91d811d7c58c1fc
Fabien Mendez <fabien.mendez@gmail.com>
2023-06-14 15:41:58 +0100

    just styled landing page

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,7 +4,7 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["ganhe mais", "lucre mais", "ajude o mundo"],
+      strings: ["ganhe mais", "ajude a sua familia", "lucre mais", "fica de boa"],
       typeSpeed: 50,
       loop: true
     })
